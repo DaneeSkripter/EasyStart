a9eef13f5bbb1974f48bf864c881aa8d74ef710b
DaneeSkripter <63862773+DaneeSkripter@users.noreply.github.com>
2022-03-24 20:28:55 +0100

    Update gpr-hack.js

--- gpr-hack.js
+++ gpr-hack.js
@@ -7,6 +7,6 @@ const file = readFileSync("./package.json", {
 
 const json = JSON.parse(file);
 
-json.name = "dis-easystart";
+json.name = "@daneeskripter/dis-easystart";
 
 writeFileSync("./package.json", JSON.stringify(json, undefined, 2));
